45a6241b8dcfff793291f1381d9a3f8570fddc47
Elizabeth Byerly <ebyerly@google.com>
2025-09-16 14:25:28 -0400

    Add separate BUILD step and save FORMAT for last
    Signed-off-by: Elizabeth Byerly <ebyerly@google.com>

--- tools/nighthawk_envoy_updater.py
+++ tools/nighthawk_envoy_updater.py
@@ -233,10 +233,11 @@ class EnvoyCommitIntegrationStep(enum.Enum):
   GET_ENVOY_SHA = enum.auto()
   SET_NIGHTHAWK_BAZEL_DEP = enum.auto()
   PATCH_SHARED_FILES = enum.auto()
-  FIX_FORMAT = enum.auto()
   BAZEL_UPDATE_REQUIREMENTS = enum.auto()
+  BUILD_NIGHTHAWK = enum.auto()
   TEST_NIGHTHAWK = enum.auto()
   UPDATE_CLI_README = enum.auto()
+  FIX_FORMAT = enum.auto()
 
 
 class EnvoyCommitIntegration(StepHandler[EnvoyCommitIntegrationStep]):
@@ -323,13 +324,13 @@ class EnvoyCommitIntegration(StepHandler[EnvoyCommitIntegrationStep]):
           raise RuntimeError(
               _patch_merge_conflict_instructions(self.target_envoy_commit,
                                                  self.nighthawk_dir)) from e
-      case EnvoyCommitIntegrationStep.FIX_FORMAT:
-        _run_command(["./ci/do_ci.sh", "fix_format"], cwd=self.nighthawk_dir)
       case EnvoyCommitIntegrationStep.BAZEL_UPDATE_REQUIREMENTS:
         _run_command(
             ["bazel", "run", "//tools/base:requirements.update"],
             cwd=self.nighthawk_dir,
         )
+      case EnvoyCommitIntegrationStep.BUILD_NIGHTHAWK:
+        _run_command(["./ci/do_ci.sh", "build"], cwd=self.nighthawk_dir)
       case EnvoyCommitIntegrationStep.TEST_NIGHTHAWK:
         _run_command(["./ci/do_ci.sh", "test"], cwd=self.nighthawk_dir)
       case EnvoyCommitIntegrationStep.UPDATE_CLI_README:
@@ -337,6 +338,8 @@ class EnvoyCommitIntegration(StepHandler[EnvoyCommitIntegrationStep]):
             ["./tools/update_cli_readme_documentation.sh", "--mode=fix"],
             cwd=self.nighthawk_dir,
         )
+      case EnvoyCommitIntegrationStep.FIX_FORMAT:
+        _run_command(["./ci/do_ci.sh", "fix_format"], cwd=self.nighthawk_dir)
       case _:
         raise ValueError(f"{step} is not supported.")
 
